32d25516e0a8b3e2567c5fcc3fae96f20183d246
kadoubleU <delievabg@gmail.com>
2023-09-01 20:05:50 +0300

    Omnifood optimizations

--- 07.OmniFood-Project/js/script.js
+++ 07.OmniFood-Project/js/script.js
@@ -1,9 +1,9 @@
 console.log("Hello, world!");
 
-const myName = "Jonas Schmedtmann";
+const myName = "Delieva.com";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
-console.log(h1);
+console.log(h1); 
 
 // h1.style.cursor = "pointer";
 
@@ -27,6 +27,64 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener('click', function() {
   headerEl.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function(link) {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    
+    //Scroll back to top
+    if(href === "#") window.scrollTo( {
+      top: 0,
+      behavior: "smooth",
+    });
+
+    //Scroll to other links
+    if(href !== "#" && href.startsWith("#")) {
+     const sectionEl =  document.querySelector(href);
+
+     sectionEl.scrollIntoView({behavior: "smooth"});
+    }
+
+    //Close mobile navigation
+    if(link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+//Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero")
+
+const obs = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+  
+  if(ent.isIntersecting === false) {
+    document.body.classList.add("sticky");
+  }
+
+  if(ent.isIntersecting === true) {
+    document.body.classList.remove("sticky");
+  }
+},
+
+{
+  //In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px",
+});
+
+obs.observe(sectionHeroEl);
+
+
 
 
 
